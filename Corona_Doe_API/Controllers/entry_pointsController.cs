7fcc2e01b741938c4cd3942ce1100c4f77f35d0b
Thawdar Myint Aung <thawdar@itstar.io>
2020-04-02 10:04:58 +0630

    Initiate controller

---
+++ Corona_Doe_API/Controllers/entry_pointsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using d = DataAccess;
+using e = Entity;
+namespace Corona_Doe_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class entry_pointsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<e.entry_points>> Get()
+        {
+            return await d.entry_points.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<e.entry_points> Get(int id)
+        {
+            return await d.entry_points.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] e.entry_points obj)
+        {
+            try
+            {
+                await d.entry_points.Insert(obj);
+
+                return Ok(obj);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] e.entry_points obj)
+        {
+            try
+            {
+                await d.entry_points.Update(obj);
+
+                return Ok(obj);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await d.entry_points.Delete(id);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
